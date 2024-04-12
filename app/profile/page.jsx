6c6c8fe52af702c1d6c11bb42727dda223a28824
brooktewabe <brooktewabe00@gmail.com>
2024-04-12 16:23:34 +0300

    'protected-routes'

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,35 +1,35 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { UserAuth } from "../context/AuthContext";
+import { useRouter } from "next/navigation";
 import Spinner from "../components/Spinner";
-import { useRouter } from 'next/navigation'
-
 
 const page = () => {
   const { user } = UserAuth();
   const [loading, setLoading] = useState(true);
-  const router = useRouter()
 
+  const router = useRouter();
 
   useEffect(() => {
     const checkAuthentication = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 50));
       setLoading(false);
+      const isAuth = localStorage.getItem("isAuth");
+
+      if (!user && !isAuth) {
+        router.push("/login");
+      }
     };
     checkAuthentication();
-  }, [user]);
-
+  }, [user, router]);
+  // console.log(user);
   return (
     <div className="p-4">
-      {loading ? (
-        <Spinner />
-      ) : user ? (
+      {loading && <Spinner/>}
+      {!loading && user && (
         <p>
           Welcome, {user.displayName} - you are logged in to the profile page -
           a protected route.
         </p>
-      ) : (
-        router.push('/')
       )}
     </div>
   );
