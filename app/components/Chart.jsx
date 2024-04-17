50c98db131ded158e374bbf94a223c2e4d687f22
brooktewabe <brooktewabe00@gmail.com>
2024-04-17 14:17:35 +0300

    'chart-and-icons-fixed'

--- app/components/Chart.jsx
+++ app/components/Chart.jsx
@@ -1,49 +1,54 @@
-'use client'
 import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const Donut = () => {
-  let [highRes, setHighRes]= useState(10)
-  let [mediumRes, setMediumRes]= useState(10)
-  let [lowRes, setLowRes]= useState(10)
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const highResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/high`
-      );
-      console.log(await highResponse.json())
-      const mediumResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/medium`
-      );
-      const lowResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/low`
-      );
-
-      setHighRes(highResponse.data);
-      setMediumRes(mediumResponse.data);
-      setLowRes(lowResponse.data);
-    };
-    fetchData();
-  }, []);
+  const [highRes, setHighRes] = useState(null);
+  const [mediumRes, setMediumRes] = useState(null);
+  const [lowRes, setLowRes] = useState(null);
+  // const [series] = useState([highRes, mediumRes, lowRes]);
   const [options] = useState({
     labels: ["High", "Medium", "Low"],
   });
-  const [series] = useState([highRes, mediumRes, lowRes]);
-console.log('hR'+ highRes)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const highResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/high`
+        );
+        const highData = await highResponse.json();
+
+        const mediumResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/medium`
+        );
+        const mediumData = await mediumResponse.json();
+
+        const lowResponse = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/tickets/priority/low`
+        );
+        const lowData = await lowResponse.json();
+
+        setHighRes(highData);
+        setMediumRes(mediumData);
+        setLowRes(lowData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <>
       <div className="donut">
         <Chart
           options={options}
-          series={series}
+          series={[highRes, mediumRes, lowRes]}
           type="pie"
           width="380"
           height="380"
         />
-        {/* <Chart options={options} series={series} type="donut" width="380" /> */}
       </div>
     </>
   );
